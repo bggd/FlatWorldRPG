45d369510dc3ca31085c9de43633c475bceed490
bggd <gameovera@live.com>
2020-12-25 19:17:27 +0900

    Add Node.propagate

--- src/node.lua
+++ src/node.lua
@@ -7,11 +7,13 @@ function Node.create()
     children = {},
 
     -- properties
+    globalPosition = Vector3.zero(),
     position = Vector3.zero(),
 
     -- callbacks
     onReady = nil,
-    onProcess = nil
+    onProcess = nil,
+    onDraw = nil
   }
   return obj
 end
@@ -26,6 +28,29 @@ end
 function Node.addChild(node, child)
   table.insert(node.children, child)
   Node.setParent(child, node)
+end
+
+function Node.propagate(node)
+  if node.onProcess then
+    assert(type(node.onProcess) == "function")
+    node:onProcess()
+  end
+
+  if node.parent then
+    local p = node.parent
+    node.globalPosition = Vector3.add(p.globalPosition, node.position)
+  else
+    node.globalPosition = node.position
+  end
+
+  if node.onDraw then
+    assert(type(node.onDraw) == "function")
+    node:onDraw()
+  end
+
+  for _, v in ipairs(node.children) do
+    Node.propagate(v)
+  end
 end
 
 return Node
--- src/sprite.lua
+++ src/sprite.lua
@@ -10,6 +10,8 @@ function Sprite.create()
   obj.flip_h = false
   obj.flip_v = false
 
+  obj.onDraw = Sprite.onDraw
+
   return obj
 end
 
@@ -17,6 +19,11 @@ function Sprite.setImage(spr, image)
   assert(image:typeOf("Image"))
 
   spr.image = image
+end
+
+function Sprite:onDraw()
+  local pos = self.globalPosition
+  love.graphics.draw(self.image, pos[1], pos[2])
 end
 
 return Sprite
